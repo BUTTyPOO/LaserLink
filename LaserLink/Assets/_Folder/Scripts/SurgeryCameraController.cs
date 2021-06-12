06eb100bf7d0f8cdf216479dc2a60d6b72a3cece
LowLevelLemmy <51277774+BUTTyPOO@users.noreply.github.com>
2021-06-12 16:44:47 -0700

    Please work

---
+++ LaserLink/Assets/_Folder/Scripts/SurgeryCameraController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SurgeryCameraController : MonoBehaviour
+{
+    [SerializeField] float horizontalSpeed = 40f;
+    [SerializeField] float vertSpeed = 40f;
+    [SerializeField] Transform cameraPivotHorizontal;
+    [SerializeField] Transform cameraPivotVert;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            RotateHorizontal();
+            RotateVertical();
+        }
+    }
+
+    void RotateHorizontal()
+    {
+        float rotAmmount = Input.GetAxisRaw("Mouse X") * horizontalSpeed;
+        cameraPivotHorizontal.Rotate(Vector3.up, rotAmmount);
+    }
+
+    void RotateVertical()
+    {
+        float rotAmmount = Input.GetAxisRaw("Mouse Y") * vertSpeed;
+        cameraPivotVert.Rotate(-Vector3.right, rotAmmount);
+    }
+}
