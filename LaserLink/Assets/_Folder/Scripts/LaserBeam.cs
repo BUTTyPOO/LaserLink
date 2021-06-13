68886983216e3fae551537e36bc8f46fbee2da47
LowLevelLemmy <51277774+BUTTyPOO@users.noreply.github.com>
2021-06-12 21:08:59 -0700

    Made LaserBeam.cs and added post proc

---
+++ LaserLink/Assets/_Folder/Scripts/LaserBeam.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserBeam : MonoBehaviour
+{
+    [SerializeField] LineRenderer lineRenderer;
+    [SerializeField] LayerMask layerMask;
+
+    void Start()
+    {
+        lineRenderer ??= GetComponent<LineRenderer>();
+    }
+
+    void Update()
+    {
+        RaycastHit hitInfo;
+        Vector3 rayOrigin = transform.TransformPoint(lineRenderer.GetPosition(0));
+        Ray ray = new Ray(rayOrigin, transform.forward);
+
+        if (Physics.Raycast(ray, out hitInfo, 1000, layerMask))
+        {
+            lineRenderer.SetPosition(1, transform.InverseTransformPoint(hitInfo.point));
+        }
+        else
+        {
+            lineRenderer.SetPosition(1, new Vector3(0, 0, 100));
+        }
+
+
+        // print(rayOrigin);
+        // GameObject abba = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        // abba.transform.localPosition = rayOrigin;
+        // abba.transform.localScale *= .05f;
+    }
+}
